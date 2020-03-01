857a7066349b5629f940691aced72f8c48bdcb9b
JetNoLi <hendricks.jet@gmail.com>
2020-03-01 17:25:39 +0200

    LSC class complete edited main

--- LSArrayApp.java
+++ LSArrayApp.java
@@ -1,30 +1,38 @@
 import java.util.Scanner;
-import java.io.File;
+import java.io.*;
 
 public class LSArrayApp{
 	//Array of LSC objects as instance variable
+	private int arrLength;
 	private LSC[] LSArray;
 
 	//constructor
 	public LSArrayApp(){
-		LSArray = {}; // empty array
+		LSArray = null;
+	       	arrLength = 0;	// empty array
 		}
 
 	public LSArrayApp(LSC[] array){
-		LSArray = Array;
+		LSArray = array;
+		arrLength = array.length;
 		}
 	
 	//methods
+	public int getLength(){
+		return arrLength;
+		}
+
 	public  void addToArray(LSC item){
-		int len = LSArray.length;
-		LSC[] newLSArray = new LSC[len+1];
-		
-		for (int i = 0; i <= len-1; i++){
+
+		LSC[] newLSArray = new LSC[arrLength+1];
+	
+		for (int i = 0; i <= arrLength-1; i++){
 			newLSArray[i] = LSArray[i];
 			}
 		
-		newLSArray[len] = item; 
+		newLSArray[arrLength] = item; 
 		LSArray = newLSArray;
+		arrLength +=1;
 		}
 	
 	public LSC[] getArray(){
@@ -33,15 +41,26 @@ public class LSArrayApp{
 
 	//main
 	public static void main(String[] args){
-		File file = new File("Load_Shedding_All_Areas_Schedule_and_Map.clean.final");// add exc file not found
-		Scanner scFile = new Scanner(file);
 		LSArrayApp LSArray = new LSArrayApp();
-		while(scFile.hasNextLine()){
-			LSC newLSC = new LSC(scFile.nextLine());
-			LSArray.addToArray(item);
+
+		try{
+			File file = new File("Load_Shedding_All_Areas_Schedule_and_Map.clean.final.txt");
+			Scanner scFile = new Scanner(file);
+			//LSArrayApp LSArray = new LSArrayApp();
+                	while(scFile.hasNextLine()){
+                        	LSC newLSC = new LSC(scFile.nextLine());
+                        	LSArray.addToArray(newLSC);
+                        	}
+
 			}
+
+		//Exception if file it not found
+		catch(FileNotFoundException e){
+			System.out.println("Error: File Not Found");
+			}
+
 		LSC[] array = LSArray.getArray();	
-		System.out.println(array[0]);
+		
 		}
 	}
 
--- LSC.java
+++ LSC.java
@@ -1,13 +1,15 @@
+import java.util.Arrays;
+
 public class LSC{
 	
 	//instance variables
-	private int stage;
+	private String stage;
         private String day;
         private String time;
-	private int[] zones;
+	private String[] zones;
 
 	//constructors
-	public LSC(int stage, String day, String time, int[] zones){
+	public LSC(String stage, String day, String time, String[] zones){
 		this.stage = stage;
 		this.day = day;
 		this.time = time;
@@ -18,24 +20,21 @@ public class LSC{
 		String[] info = key.split(" ",2);
                 String[] varArray = info[0].split("_");
                 String[] zoneArray = info[1].split(", ");
-                int[] zoneA = new int[zoneArray.length];
-                for (int i = 0; i < zoneArray.length; i++){
-                        zoneA[i] = Integer.parseInt(zoneArray[i]);
-                        }
-		stage = Integer.parseInt(varArray[0]);
+                
+		stage = varArray[0];
 		day = varArray[1];
 		time = varArray[2];
-		this.zones = zoneA;
+		this.zones = zoneArray;
 		}
 
 	
 	
 	//methods
 	public String toString(){
-                return "Stage: "+ stage + " Day: " + day + " Time " + time + " Zones: " + zones;
+                return "Stage: "+ stage + " Day: " + day + " Time " + time + " Zones: " + Arrays.toString(zones);
                 }
 
-        public int getStage(){
+        public String getStage(){
                 return stage;
                 }
 
