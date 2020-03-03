e82d805ff86e40f6ff6dc51f69907e7c3e804301
JetNoLi <hendricks.jet@gmail.com>
2020-03-04 01:14:46 +0200

    ompleted Part 5 script just need to adjust output to increment

--- Test.py
+++ Test.py
@@ -3,29 +3,33 @@ import subprocess
 originalFile = open("Load_Shedding_All_Areas_Schedule_and_Map.clean.final.txt","r")
 fileLines = originalFile.readlines()
 
-fileNameOrig = "LSArrayAppdataset_.txt"
-cmdInputOrig = "java LSArrayApp _"
+fileNameOrig = "LSArrayAppDataSet_.txt"
+cmdAppType = "LSArrayApp"
 
-    
-for i in range(1,11):
-    fileName = fileNameOrig.replace("_", str(i))
-    with open(fileName, "w") as f:
-        print(fileName)
-        for x in range(2976):
-            if (x % i == 0):
-                data = fileLines[x]
-                data = data[0:7]
+for y in range (2):
+    if (y == 1):
+        cmdAppType = "LSBSTApp"
+        fileNameOrig = "LSBSTAppDataSet_.txt"
+
+    for i in range(1,11):
+        fileName = fileNameOrig.replace("_", str(i))
+        with open(fileName, "w") as f:
+            print(fileName)
+            for x in range(2976):
+                if (x % i == 0):
+                    data = fileLines[x]
+                    data = data[0:7]
                 
-                if (data[6] == " "):
-                    data = data[0:6]
+                    if (data[6] == " "):
+                        data = data[0:6]
                
-                data = data.split("_")
+                    data = data.split("_")
 
 
-                cmdInput = ["java", "LSArrayApp",data[0],data[1],data[2]] 
+                    cmdInput = ["java", cmdAppType,data[0],data[1],data[2]] 
 
-                
-                subprocess.run(cmdInput, stdout = f, text = True)
+                    print("progress %i",x)                
+                    subprocess.run(cmdInput, stdout = f, text = True)
                 
 
 
